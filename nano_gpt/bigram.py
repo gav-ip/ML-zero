3afef9d7e240d1b6c76bd448445d762d1e9f3ac5
Gavin Abrigo <135771042+gav-ip@users.noreply.github.com>
2026-08-08 23:05:01 -0700

    adding self-attention head

--- nano_gpt/bigram.py
+++ nano_gpt/bigram.py
@@ -18,8 +18,8 @@ import matplotlib.pyplot as plt
 # hyperparameters
 batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maximum context length for predictions?
-max_iters = 3000
-eval_interval = 300
+max_iters = 5000
+eval_interval = 500
 learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'using device: {device}')
@@ -67,20 +67,41 @@ def estimate_loss():
   model.train()
   return out
 
+class Head(nn.Module):
+  def __init__(self, head_size):
+    super().__init__()
+    self.key = nn.Linear(n_embd, head_size, bias=False)
+    self.query = nn.Linear(n_embd, head_size, bias=False)
+    self.value = nn.Linear(n_embd, head_size, bias=False)
+    self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+    
+  def forward(self, x):
+    B, T, C = x.shape
+    k = self.key(x)
+    q = self.query(x)
+    wei = q @ k.transpose(-2, -1) * C**-0.5
+    wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+    wei = F.softmax(wei, dim=-1)
+    v = self.value(x)
+    return wei @ v
 
 class BigramLanguageModel(nn.Module):
   def __init__(self):
     super().__init__()
     self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
     self.position_embedding_table = nn.Embedding(block_size, n_embd)
+    self.sa_head = Head(n_embd)
     self.lm_head = nn.Linear(n_embd, vocab_size)
-    
+  
   def forward(self, idx, targets=None):
+    
+    B, T = idx.shape
     tok_emb = self.token_embedding_table(idx) # (B,T,C)
     pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
     
     x = tok_emb + pos_emb # (B,T,C)
-    
+    x = self.sa_head(x)
+
     logits = self.lm_head(x) # (B,T,vocab_size)
     
     if targets is None:
@@ -96,11 +117,17 @@ class BigramLanguageModel(nn.Module):
   def genereate(self, idx, max_new_tokens):
 
     for _ in range(max_new_tokens):
-
-      logits, loss = self(idx)
+      # crop context window to block_size tokens
+      idx_cond = idx[:, -block_size:]
+      # get logits and loss for the current context window
+      logits, loss = self(idx_cond)
+      # focus only on the last time step
       logits = logits[:, -1, :]
+      # apply softmax to get probabilities
       softmax = F.softmax(logits, dim=-1)
+      # sample from the distribution
       idx_next = torch.multinomial(softmax, num_samples=1)
+      # append sampled index to the running sequence
       idx = torch.cat((idx, idx_next), dim=1)
     return idx
 
