50464676002b4c02272afc6c0a18f9efac714b2b
Gavin Abrigo <135771042+gav-ip@users.noreply.github.com>
2026-08-11 10:44:51 -0700

    adding dropout and layernorm

--- nano_gpt/attention.py
+++ nano_gpt/attention.py
@@ -16,17 +16,18 @@ import matplotlib.pyplot as plt
 # %matplotlib inline
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'using device: {device}')
 eval_iters = 200
-n_head = 4
-n_layer = 4
-n_embd = 32
+n_head = 6
+n_layer = 6
+n_embd = 384
+dropout = 0.2
 
 text = open('input.txt', 'r', encoding='utf-8').read()
 
@@ -77,6 +78,7 @@ class Head(nn.Module):
     self.query = nn.Linear(n_embd, head_size, bias=False)
     self.value = nn.Linear(n_embd, head_size, bias=False)
     self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+    self.dropout = nn.Dropout(dropout)
     
   def forward(self, x):
     B, T, C = x.shape
@@ -85,6 +87,7 @@ class Head(nn.Module):
     wei = q @ k.transpose(-2, -1) * C**-0.5
     wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
     wei = F.softmax(wei, dim=-1)
+    wei = self.dropout(wei)
     v = self.value(x)
     return wei @ v
 
@@ -93,10 +96,12 @@ class MultiHeadAttention(nn.Module):
     super().__init__()
     self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
     self.proj = nn.Linear(n_embd, n_embd)
+    self.dropout = nn.Dropout(dropout)
   
   def forward(self, x):
     out = torch.cat([head(x) for head in self.heads], dim=-1)
-    return self.proj(out)
+    out = self.dropout(self.proj(out))
+    return out
 
 class FeedForward(nn.Module):
   def __init__(self, n_embd):
@@ -105,6 +110,7 @@ class FeedForward(nn.Module):
       nn.Linear(n_embd, 4 * n_embd),
       nn.ReLU(),
       nn.Linear(4 * n_embd, n_embd),
+      nn.Dropout(dropout),
     )
   def forward(self, x):
     return self.net(x)
@@ -116,10 +122,12 @@ class Block(nn.Module):
     head_size = n_embd // n_head
     self.sa = MultiHeadAttention(num_heads=4, head_size=n_embd // 4)
     self.ff = FeedForward(n_embd)
+    self.ln1 = nn.LayerNorm(n_embd)
+    self.ln2 = nn.LayerNorm(n_embd)
   
   def forward(self, x):
-    x = x + self.sa(x)
-    x = x + self.ff(x)
+    x = x + self.sa(self.ln1(x))
+    x = x + self.ff(self.ln2(x))
     return x
 
 class BigramLanguageModel(nn.Module):
@@ -127,7 +135,10 @@ class BigramLanguageModel(nn.Module):
     super().__init__()
     self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
     self.position_embedding_table = nn.Embedding(block_size, n_embd)
-    self.blocks = nn.Sequential(*[Block(n_embd, n_head) for _ in range(n_layer)])
+    self.blocks = nn.Sequential(
+      *[Block(n_embd, n_head) for _ in range(n_layer)],
+      nn.LayerNorm(n_embd),
+    )
     self.lm_head = nn.Linear(n_embd, vocab_size)
   
   def forward(self, idx, targets=None):
@@ -171,6 +182,7 @@ m = model.to(device)
 
 optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
 
+# train eval loop
 for steps in range(max_iters):
   
   if steps % eval_interval == 0:
